0bd8cc4705500f3800e32d8a6f6bafff67f2142a
unknown <Noel.Duckworth@Colorado.edu>
2025-02-06 19:07:54 -0500

    saving before I ruin things on LKF

--- +Filters/LinearKF.m
+++ +Filters/LinearKF.m
@@ -1,4 +1,4 @@
-function [xhist, measResHist] = LinearKF(IC, pert, P0, R, yHist, yHistRef, stationECI, visibilityMask, tVec, mu, J2, Re)
+function [xhist, measResHist, measDeltaHist, estimatedDeviationOb, statNumObHist, covPlus] = LinearKF(IC, pert, P0, R, yHist, yHistRef, stationECI, visibilityMask, tVec, mu, J2, Re)
 
 %%%%%%%%%%%%%% INPUTS: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % IC:           [6 x 1] Initial Total State condition that we'll propagate with
@@ -19,23 +19,9 @@ function [xhist, measResHist] = LinearKF(IC, pert, P0, R, yHist, yHistRef, stati
 % covPriorHist:  [] History of pre-measurement state uncertainty
 % measResidHist: [] History of measurement residuals/innovations
 
-%--- Integrate ref traj & STM before running filter ---
-
-
-% Set integrator options
-odeOptions = odeset('AbsTol',1e-12,'RelTol', 1e-12);
-
-% Integrate Trajectory
-[T, TrajNom] = ode45(@Dynamics.NumericJ2Prop, tVec, IC, odeOptions, mu, J2, Re);
-
-% Extract the reference trajectory states
-refTrajStates = TrajNom(:,1:6);
-
-% Extract the Integrated STM - STM is by Row!
-phi = TrajNom(:,7:end);
 
 % Function to help sort measurements
-[computedMeas, observedMeas] = Measurements.FilterMeasLoadIn(yHist);
+[computedMeas, observedMeas] = Measurements.FilterMeasLoadIn(yHist, yHistRef);
 
 % --- Implement LKF Algorithm ---
 % Naming convention
@@ -46,22 +32,43 @@ phi = TrajNom(:,7:end);
 % set filter initial conditions
 xhatPrev = pert;
 pPrev = P0;
+refState = IC;
 timePrev = 0;
 
 % Set filter to loop over number of observations
-for i = 1:length(tVec)
+for i = 1:length(tVec)-1
+        
+    %--- Integrate ref traj & STM between each time step---
+    % Set integrator options
+    odeOptions = odeset('AbsTol',1e-12,'RelTol', 1e-12);
     
-    % Setup phi for run
+    % Integrate Trajectory
+    [T, TrajNom] = ode45(@Dynamics.NumericJ2Prop, [tVec(i):tVec(i+1)], refState, odeOptions, mu, J2, Re);
+    
+    % Extract the reference trajectory states
+    refTrajStates = TrajNom(1:end,1:6);
+    
+    % Extract the Integrated STM - STM is by Row!
+    phi = TrajNom(:,7:end);
+    
+    
+    
+    
+    
+    
+    
     if i == 1
-        % For first go phi is IC
+        % just grab STM from propagated ref traj
         STM = reshape(phi(i,:), [6,6]);
-        
     else
-        % After first go phi multiplies itself
-        STM = reshape(phi(i,:), [6,6])*reshape(phi(i-1, :), [6,6]);
+        % Want phi(ti, ti-1) on each iteration
         
+        % invert previous STM
+        prevSTMinv = inv(reshape(phi(i, :), [6,6]));
+        
+        % new STM maps from prev time step to current
+        STM = reshape(phi(i+1,:), [6,6]) * prevSTMinv;
     end
-    
     
     % --- Time Update Step
     xMinus = STM * xhatPrev;
@@ -76,22 +83,40 @@ for i = 1:length(tVec)
         Htilde{i} = Measurements.HtildeSC(refTrajStates(i,:)', stationECI{i,statNumOb});
         
         % --- Kalman Gain
-        Kk = pMinus * Htilde{i}' * inv(Htilde{i} * pMinus * Htilde{i}' + R);
+        Kk = pMinus * Htilde{i}' / (Htilde{i} * pMinus * Htilde{i}' + R);
         
         % -- Measurement Update
-        measRes = (measDelta(:,statNumOb) - Htilde{i}*xMinus);
+        measRes = measDelta(:,statNumOb) - Htilde{i}*xMinus;
         xhatPlus = xMinus + Kk * measRes;
-        Pplus = (eye(6,6) - Kk*Htilde{i}) * pPrev * (eye(6,6) - Kk*Htilde{i})' + Kk*R*Kk';
+        Pplus = (eye(6,6) - Kk*Htilde{i}*STM) * pPrev * (eye(6,6) - Kk*Htilde{i}*STM)' + Kk*R*Kk';
+        % Pplus = (eye(6,6) - Kk*Htilde{i}) * pMinus;
+        
+        % CHECKING P IS GETTING SMALLER
+        % assert(trace(Pplus) < trace(pMinus), 'Trace of P is not getting smaller');
+        
+        % debugging saving this
+        estimatedDeviationOb{i} = Htilde{i}*xMinus;
     else
         % no station observation - simply propagating the state w/o meas
         xhatPlus = xMinus;
         Pplus    = pMinus;
+        
+        % debugging saving this
+        estimatedDeviationOb{i} = [NaN; NaN];
+        measResHist(:,i) = [NaN; NaN];
         
     end
     
     % save off histories
     xhist(:, i) = xhatPlus;
     measResHist(:,i) = measRes;
+    covMins{i} = pMinus;
+    covPlus{i} = Pplus;
+    measDeltaHist{i} = measDelta;
+    if isempty(statNumOb)
+        statNumOb = 0;
+    end
+    statNumObHist(i) = statNumOb;
     
     % update variables for next go around
     xhatPrev = xhatPlus;
