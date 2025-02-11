1f7dba716897ba2081f035b7ee27c8bd3d09cda9
unknown <Noel.Duckworth@Colorado.edu>
2025-02-10 21:16:49 -0500

    done with hw2

--- +Const/OrbConst.m
+++ +Const/OrbConst.m
@@ -2,9 +2,10 @@ classdef OrbConst
     properties (Constant)
         % Define your constants here
         % SpeedOfLight = 299792458; % m/s
-        muEarth     = 3.986004415*10^5; % km^3/s^2
+        muEarth     = 398600.4415; % km^3/s^2
         muSun       = 1.32712428*10^11; 
-        EarthRadius = 6371; %km
+        EarthRadius = 6378.13; %km
         J2          = 0.00108248; 
+        J3          = -0.0000025323; 
     end
 end
